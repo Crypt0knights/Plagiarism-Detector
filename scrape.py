21e13a258d2e915e2e72582495a328a37262ead3
code-monk08 <mayank.singh081997@gmail.com>
2019-05-06 03:09:10 +0530

    Added scraped.py

--- scrape.py
+++ scrape.py
@@ -1,6 +1,32 @@
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 import urllib.request
+import json
+from scripts import search
+
+
+
+
+def simple_get(url):
+        try:
+                with closing(get(url, stream = True)) as response:
+                        if is_good_response(response):
+                                return  response.content
+                        else:
+                                return None
+        except RequestException as e:
+                log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+                return None
+
+def is_good_response(response):
+        content_type = response.headers['Content-Type'].lower()
+        return (response.status_code == 200
+                and content_type is not None
+                and content_type.find('html') > -1)
+
+def log_error(e):
+        print(e)
+
 
 
 def tag_visible(element):
@@ -17,8 +43,21 @@ def text_from_html(body):
     visible_texts = filter(tag_visible, texts)
     return u" ".join(t.strip() for t in visible_texts)
 
-html = urllib.request.urlopen('http://www.nytimes.com/2009/12/21/us/21storm.html').read()
-f = open("demofile2.txt", "w")
-f.write(text_from_html(html).upper())
-f.close()
-print("Done")
+
+def scrape_text():
+        fd = open('url_percent.json','r')
+        data = json.load(fd)
+        print(data)
+        for urls in data.values():
+                print(urls)
+                html = urllib.request.urlopen(urls).read()
+                f = open("scraped.txt", "w")
+                path_scraped = './scraped.txt'
+                f.write(text_from_html(html).upper())
+                f.close()
+                path_source = './check/files/search.txt'
+                search.ss(path_source, path_scraped)
+                print("Done")
+
+if __name__ == '__main__':
+    scrape_text()
