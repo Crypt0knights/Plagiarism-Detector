a48b4fcb7711a89e60247e015d656e28196dd354
code-monk08 <mayank.singh081997@gmail.com>
2019-05-06 01:16:20 +0530

    change

--- sort.py
+++ sort.py
@@ -1,12 +1,25 @@
+import json
+
 def main():
 	f=open('./check/output.json')
-	data = json.loads(f)
-	max = 0
-	list = []
-	for it in data['match_percents']:
-		if (it>=max):
-			list.append(it)
+	data = json.load(f)
+	urls={}
+	final= {}
+
+	for i in range(len(data)):
+		urls[data[i]['match_percents']]=data[i]['match_url']
+	
+	sort_percent = sorted(urls, reverse = True)[:5]
+	print(sort_percent)
+
+	for key in sort_percent:
+		final[key] = urls[key]
+
+	with open('url_percent.json','w') as url_json_file:
+		json.dump(final, url_json_file,sort_keys=True,indent=5)
+
 	f.close()
+
 
 if __name__ == '__main__':
 	main()
